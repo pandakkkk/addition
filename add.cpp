9a1570b742f2b0f05d011f794d1379a8583caccf
pandakkkk <sanjeev.murmu.mat17@itbhu.ac.in>
2019-05-08 18:49:06 +0530

    addition function

---
+++ add.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int sum(int, int);
+
+int main()
+{
+	int x,y,s;
+	cout<<"Enter first number : ";
+	cin>>x;
+	cout<<"Enter second number : ";
+	cin>>y;
+	s=sum(x,y);
+	cout<<"The sum is : "<<s;
+
+	
+	return 0;
+}
+
+int sum(int a, int b)
+{
+
+	int total;
+	total=a+b;
+	return total;
+}
+
+
